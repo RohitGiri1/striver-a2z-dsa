1afe1d2dd5b4dba2a46176f3395051ae1da2c9cd
rohit giri <rk6280840221@gmail.com>
2026-08-30 19:35:36 +0530

    right triangle pattern

---
+++ 01-Basics/patterns/pattern_2.py
@@ -0,0 +1,16 @@
+"""
+Problem: Pattern 2 - Right Trianlge Pattern
+Link: https://takeuforward.org/plus/dsa/problems/pattern-2
+
+Time Complexity: O(n^2)  -> outer loop n times, inner loop n times
+Space Complexity: O(1)   -> No extra space is used
+"""
+
+def pattern(n):
+    for i in range(1,n):
+        for j in range(0,i):
+            print("*",end = "")
+
+        print()
+
+pattern(5)
