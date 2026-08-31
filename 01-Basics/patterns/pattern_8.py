c6ebad3f6f300c26be879bb00be5b253a934ba99
rohit giri <rk6280840221@gmail.com>
2026-08-31 22:57:11 +0530

    pattern 8

---
+++ 01-Basics/patterns/pattern_8.py
@@ -0,0 +1,23 @@
+"""
+Problem 8 - Reverse Pyramid
+Link : https://takeuforward.org/plus/dsa/problems/pattern-8
+Time Complexity : O(n^2)
+Space Complexity : O(1)
+"""
+
+def pattern(n):
+    for i in range(n):
+        for j in range(i):
+            print(" ",end="")
+        for k in range(((n-i)*2)-1):
+            print("*",end="")
+        print()
+
+# Test
+pattern(4)
+
+# Output
+# *******
+#  *****
+#   ***
+#    *
